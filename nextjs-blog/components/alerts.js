9233d03141985c9812129bd654665a6d5cf18cc0
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-05-17 12:23:48 -0400

    add clsx

---
+++ nextjs-blog/components/alerts.js
@@ -0,0 +1,15 @@
+import styles from './alert.module.css';
+import { clsx } from 'clsx';
+
+const Alert = ({ children, type }) => (
+  <div
+    className={clsx({
+      [styles.success]: type === 'success',
+      [styles.error]: type === 'error',
+    })}
+  >
+    {children}
+  </div>
+);
+
+export default Alert;
