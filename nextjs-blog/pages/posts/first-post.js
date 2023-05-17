04c55a979adc3a6404a21a41322a668adce56497
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-05-17 11:39:10 -0400

    add head react component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const FirstPost = () => (
   <>
+    <Head>
+      <title>First Post</title>
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href='/'>Back to home</Link>
