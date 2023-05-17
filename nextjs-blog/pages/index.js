96879b8f2fdd3bde8868674d3ec27358313d5ffa
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-05-17 11:43:53 -0400

    learn next/script

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -55,10 +55,7 @@ export default function Home() {
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel' className={styles.logo} />
-        </a>
+        ></a>
       </footer>
 
       <style jsx>{`
--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,11 +1,23 @@
 import Link from 'next/link';
 import Head from 'next/head';
+// next/script simplifies loading third-party scripts
+import Script from 'next/script';
 
 const FirstPost = () => (
   <>
     <Head>
       <title>First Post</title>
     </Head>
+    <Script
+      src='https://connect.facebook.net/en_US/sdk.js'
+      // strategy controls when the third-party script should load
+      // lazyOnload tells next to load this script lazily during browser idle time
+      strategy='lazyOnload'
+      // onLoad is used to run ay JS code immediately after the script has finished loading
+      onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+      }
+    />
     <h1>First Post</h1>
     <h2>
       <Link href='/'>Back to home</Link>
