20138074ea3443b31b0da8b94f762c822b3ee928
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-05-25 10:37:36 -0400

    adding dynamic routes

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,6 +1,8 @@
 import fs from 'fs'; // node module that lets you read files from the file system
 import path from 'path'; // module that lets you manipulate file paths
 import matter from 'gray-matter'; // lib that lets your parse the metadata in each markdown file
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -45,16 +47,23 @@ export function getAllPostIds() {
   });
 }
 
-export function getPostData(id) {
+export async function getPostData(id) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
   // Combine the data with the id
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   };
 }
